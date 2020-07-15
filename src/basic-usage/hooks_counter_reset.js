97453d8350966f66376e02b40c7706a03a726079
YanPanMichael <panyancsu@163.com>
2020-07-15 19:56:38 +0800

    version ref 2

--- src/basic-usage/hooks_counter_reset.js
+++ src/basic-usage/hooks_counter_reset.js
@@ -1,26 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
-import { EventEmitter } from 'fbemitter';
+// import { EventEmitter } from 'fbemitter';
 
-const emitter = new EventEmitter();
-window.emitter = emitter;
-const EventName = 'EVENT_NAME';
+// const emitter = new EventEmitter();
+// window.emitter = emitter;
+// const EventName = 'EVENT_NAME';
 
 const CounterReset = () => {
   const [count, setCount] = useState(c => c = 0);
+  const ref = useRef();
+  if (count > 5) {
+    console.log('outer', count);
+  }
   useEffect(() => {
-    const handler = () => {
+    if(count > 5) {
+      console.log('inner', count);
+      console.log(ref.current.innerText);
       setCount(0);
     }
-    emitter.addListener(EventName, handler);
-    return () => {
-      emitter.removeAllListeners(EventName);
-    }
-  }, []);
+  }, [count]);
   return (
     <div>
       <div className="desc-list" onClick={() => setCount(c => c + 1)}>
-        计数 {count} ; 点击后递增
+        计数 <span ref={ref}> {count} </span> ; 点击后递增
       </div>
       <div className="desc-list" onClick={() => emitter.emit(EventName)}>
         点击后重置
